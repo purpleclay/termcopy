d4570cbf3370ac9abd679208f3b3a5f421366069
Purple Clay <106762954+purpleclay@users.noreply.github.com>
2022-07-21 08:14:39 +0100

    feat: xterm support for copying to clipboard using osc (#3)

---
+++ copy.go
@@ -0,0 +1,86 @@
+/*
+Copyright (c) 2022 Purple Clay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package clipboard
+
+import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	termEnv = "TERM"
+	xterm   = "xterm"
+)
+
+var out = os.Stdout
+
+// Supported identifies whether the current terminal supports copying
+// to the system clipboard.
+func Supported() bool {
+	var termType string
+	if termType = os.Getenv(termEnv); termType == "" {
+		return false
+	}
+
+	return strings.HasPrefix(termType, xterm)
+}
+
+// Copy the contents of the reader to the system clipboard. If the current
+// terminal is not supported the operating system code (OSC) will be
+// ignored and the existing system clipboard will remain unmodified
+func Copy(in io.Reader) {
+	buf := bufio.NewWriter(out)
+	fmt.Fprint(buf, "\033]52;c;")
+
+	enc := base64.NewEncoder(base64.StdEncoding, buf)
+	io.Copy(enc, in)
+	enc.Close()
+	fmt.Fprint(buf, "\a")
+
+	buf.Flush()
+}
+
+// CopyBytes copies the contents of a byte array to the system clipboard.
+// If the current terminal is not supported the operating system code (OSC)
+// will be ignored and the existing system clipboard will remain unmodified
+func CopyBytes(in []byte) {
+	b64 := base64.StdEncoding.EncodeToString(in)
+
+	buf := bufio.NewWriter(out)
+	fmt.Fprint(buf, "\033]52;c;")
+	fmt.Fprint(buf, b64)
+	fmt.Fprint(buf, "\a")
+
+	buf.Flush()
+}
+
+// CopyString copies the contents of a string to the system clipboard.
+// If the current terminal is not supported the operating system code (OSC)
+// will be ignored and the existing system clipboard will remain unmodified
+func CopyString(in string) {
+	CopyBytes([]byte(in))
+}
---
+++ copy_test.go
@@ -0,0 +1,98 @@
+/*
+Copyright (c) 2022 Purple Clay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package clipboard
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSupported(t *testing.T) {
+	setEnv(t, termEnv, "xterm-256color")
+
+	assert.True(t, Supported())
+}
+
+func TestSupported_TermEnvNotSet(t *testing.T) {
+	setEnv(t, termEnv, "")
+
+	assert.False(t, Supported())
+}
+
+func setEnv(t *testing.T, key, value string) {
+	t.Helper()
+
+	curEnv := os.Getenv(key)
+	os.Setenv(key, value)
+
+	t.Cleanup(func() {
+		os.Setenv(key, curEnv)
+	})
+}
+
+func TestCopy(t *testing.T) {
+	out = tempFile(t)
+
+	Copy(strings.NewReader("this is a test"))
+
+	cb, err := ioutil.ReadFile(out.Name())
+	require.NoError(t, err)
+
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+}
+
+func TestCopyBytes(t *testing.T) {
+	out = tempFile(t)
+
+	CopyBytes([]byte("this is a test"))
+
+	cb, err := ioutil.ReadFile(out.Name())
+	require.NoError(t, err)
+
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+}
+
+func TestCopyString(t *testing.T) {
+	out = tempFile(t)
+
+	CopyString("this is a test")
+
+	cb, err := ioutil.ReadFile(out.Name())
+	require.NoError(t, err)
+
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+}
+
+func tempFile(t *testing.T) *os.File {
+	t.Helper()
+
+	f, err := ioutil.TempFile(t.TempDir(), "test")
+	require.NoError(t, err)
+
+	return f
+}

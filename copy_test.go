58f6f4d943eb2416e1fa5cf57245a4ae39b13c52
Purple Clay <106762954+purpleclay@users.noreply.github.com>
2022-07-22 13:07:35 +0100

    fix: add missing checks for tmux and apple terminal (#8)

--- copy_test.go
+++ copy_test.go
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package termcopy
+package termcopy_test
 
 import (
 	"io/ioutil"
@@ -28,20 +28,38 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/purpleclay/termcopy"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSupported(t *testing.T) {
-	setEnv(t, termEnv, "xterm-256color")
+	t.Run("XTERM", func(t *testing.T) {
+		setEnv(t, "TERM", "xterm-256color")
+		assert.True(t, termcopy.Supported())
+	})
 
-	assert.True(t, Supported())
-}
+	t.Run("Screen", func(t *testing.T) {
+		setEnv(t, "TERM", "screen-256color")
+		assert.False(t, termcopy.Supported())
+	})
 
-func TestSupported_TermEnvNotSet(t *testing.T) {
-	setEnv(t, termEnv, "")
+	t.Run("TMUX", func(t *testing.T) {
+		setEnv(t, "TERM", "xterm-256color")
+		setEnv(t, "TMUX", "/private/tmp/tmux-502/default,33442,0")
+		assert.False(t, termcopy.Supported())
+	})
 
-	assert.False(t, Supported())
+	t.Run("AppleTerminal", func(t *testing.T) {
+		setEnv(t, "TERM", "xterm-256color")
+		setEnv(t, "TERM_PROGRAM", "Apple_Terminal")
+		assert.False(t, termcopy.Supported())
+	})
+
+	t.Run("NoTERM", func(t *testing.T) {
+		setEnv(t, "TERM", "")
+		assert.False(t, termcopy.Supported())
+	})
 }
 
 func setEnv(t *testing.T, key, value string) {
@@ -56,43 +74,47 @@ func setEnv(t *testing.T, key, value string) {
 }
 
 func TestStream(t *testing.T) {
-	out = tempFile(t)
+	out := captureStdout(t, func() {
+		termcopy.Stream(strings.NewReader("this is a test"))
+	})
 
-	Stream(strings.NewReader("this is a test"))
-
-	cb, err := ioutil.ReadFile(out.Name())
-	require.NoError(t, err)
-
-	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", out)
 }
 
 func TestBytes(t *testing.T) {
-	out = tempFile(t)
+	out := captureStdout(t, func() {
+		termcopy.Bytes([]byte("this is a test"))
+	})
 
-	Bytes([]byte("this is a test"))
-
-	cb, err := ioutil.ReadFile(out.Name())
-	require.NoError(t, err)
-
-	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", out)
 }
 
 func TestString(t *testing.T) {
-	out = tempFile(t)
+	out := captureStdout(t, func() {
+		termcopy.String("this is a test")
+	})
 
-	String("this is a test")
-
-	cb, err := ioutil.ReadFile(out.Name())
-	require.NoError(t, err)
-
-	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", string(cb))
+	assert.Equal(t, "\033]52;c;dGhpcyBpcyBhIHRlc3Q=\a", out)
 }
 
-func tempFile(t *testing.T) *os.File {
+func captureStdout(t *testing.T, capture func()) string {
 	t.Helper()
 
+	// Replace stdout with a temporary file for capturing output
 	f, err := ioutil.TempFile(t.TempDir(), "test")
 	require.NoError(t, err)
 
-	return f
+	stdout := os.Stdout
+	os.Stdout = f
+
+	t.Cleanup(func() {
+		os.Stdout = stdout
+	})
+
+	capture()
+
+	cb, err := ioutil.ReadFile(f.Name())
+	require.NoError(t, err)
+
+	return string(cb)
 }
